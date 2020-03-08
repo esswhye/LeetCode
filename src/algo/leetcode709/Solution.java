dfe5188c21c0f3695eb3a109802785427bcaf919
esswhye <chansengyong@live.com>
2020-03-09 02:20:47 +0800

    709

---
+++ src/algo/leetcode709/Solution.java
@@ -0,0 +1,12 @@
+package algo.leetcode709;
+
+public class Solution {
+    public static void main(String[] args) {
+        String hello = "HELLO";
+        toLowerCase(hello);
+    }
+    public static String toLowerCase(String str) {
+        str = str.toLowerCase();
+        return str;
+    }
+}
