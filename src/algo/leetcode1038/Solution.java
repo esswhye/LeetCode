33ad1e9da398f59fe3b8bc557bcadddac4056815
esswhye <chansengyong@live.com>
2020-03-08 20:46:34 +0800

    1380

--- src/algo/leetcode1038/Solution.java
+++ src/algo/leetcode1038/Solution.java
@@ -1,24 +1,57 @@
 package algo.leetcode1038;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
     //Input: [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
     //Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
 
+    int sum = 0;
     public static void main(String[] args) {
 
         int arr[] = {4,1,6,0,2,5,7,3,8};
         TreeNode root = new TreeNode(arr);
 
-        int totalSum = root.calculateSum(root);
-        root.inOrderTraverseGST(root,totalSum);
-        TreeNode.inOrderTraverse(root);
+
+        Solution s = new Solution();
+        //s.bstToGst(root);
+
+        root.BFS(root);
 
     }
 
-    public TreeNode bstToGst(TreeNode root) {
-        return null;
+    //Traverse from left the right on a sorted element
+    public TreeNode bstToGst(TreeNode node) {
+
+        if(node.right!=null)
+        {
+            bstToGst(node.right);
+        }
+
+        sum += node.val;
+        node.val= sum;
+
+        if(node.left!=null)
+        {
+            bstToGst(node.left);
+        }
+        return node;
     }
+    /*
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode node) {
+        if(node == null) return null;
+
+        bstToGst(node.right);
+        sum += node.val;
+        node.val= sum;
+        bstToGst(node.left);
+
+        return node;
+    }
+     */
 }
 
 class TreeNode{
@@ -92,29 +125,6 @@ class TreeNode{
         }
     }
 
-    //{0,1,2,3,4,5,6,7,8}
-    public int inOrderTraverseGST(TreeNode node, int totalSum)
-    {
-        int left, right;
-        left = right = 0;
-
-        if(node.left != null)
-        {
-            inOrderTraverseGST(node.left,totalSum);
-        }
-
-        int temp = node.val;
-        node.val += totalSum;
-        totalSum=-temp;
-
-        if(node.right != null)
-        {
-            inOrderTraverseGST(node.right,totalSum);
-        }
-
-        return totalSum;
-    }
-
     public int calculateSum(TreeNode temp){
         int sum, sumLeft, sumRight;
         sum = sumRight = sumLeft = 0;
@@ -131,5 +141,24 @@ class TreeNode{
         return sum;
     }
 
+
+    public void BFS(TreeNode node)
+    {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+        TreeNode nodeToAdd;
+
+        while(!queue.isEmpty()){
+            nodeToAdd = queue.poll();
+            if(nodeToAdd.left != null)
+            {
+             queue.add(nodeToAdd.left);
+            }
+            if(nodeToAdd.right != null)
+            {
+                queue.add(nodeToAdd.right);
+            }
+        }
+     }
 }
 
