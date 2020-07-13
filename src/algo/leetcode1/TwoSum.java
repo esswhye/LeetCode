96018bd63fa421319015588f46575e120f9ae6c8
SY-Home <chansengyong@live.com>
2020-07-14 03:13:48 +0800

    TwoSum

---
+++ src/algo/leetcode1/TwoSum.java
@@ -0,0 +1,44 @@
+package algo.leetcode1;
+
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+[2,5,5,11]
+10
+[0,4,3,0]
+0
+[-1,-2,-3,-4,-5]
+-8
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        twoSum(new int[]{-1, -2, -3, -4, -5}, -8);
+        System.out.println(-8 / 2);
+    }
+
+    //BruteForce
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+
+}
