db1577554312c48f9604cb3e8324f269d2d1afcc
esswhye <chansengyong@live.com>
2020-09-17 12:49:10 +0800

    421. Maximum XOR of Two Numbers in an Array 198. House Robber

--- src/septchallenge/MaximumXORofTwoNumbersinanArray.java
+++ src/septchallenge/MaximumXORofTwoNumbersinanArray.java
@@ -20,11 +20,96 @@ Explanation: The maximum result is 5 ^ 25 = 28.
 
  */
 public class MaximumXORofTwoNumbersinanArray {
-    public int findMaximumXOR(int[] nums) {
-        int max = 0;
 
-        
+    static Trie trie = new Trie();
+
+
+    public static void main(String[] args) {
+
+        findMaximumXOR(new int[]{3, 10, 5, 25, 2, 8});
+    }
+
+    public static int findMaximumXOR(int[] nums) {
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums
+        ) {
+            insert(num);
+        }
+
+        for (int num : nums) {
+            Trie curr = trie;
+            int cnum = 0;
+
+
+            for (int i = 31; i >= 0; i--) {
+
+                int bit = (num >> i) & 1;
+
+                if (bit == 1) {
+                    if (curr.child[0] != null) {
+
+                        cnum = cnum | (1 << i);
+                        curr = curr.child[0];
+
+                    } else {
+                        curr = curr.child[1];
+                    }
+                } else {
+                    if (curr.child[1] != null) {
+                        cnum = cnum | (1 << i);
+                        curr = curr.child[1];
+                    } else {
+                        curr = curr.child[0];
+                    }
+                }
+
+
+            }
+
+            max = Math.max(max, cnum);
+
+
+        }
 
         return max;
     }
+
+    private static void insert(int num) {
+
+        Trie curr = trie;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if (curr.child[bit] == null) {
+                curr.child[bit] = new Trie();
+            }
+            curr = curr.child[bit];
+
+        }
+
+    }
+
+    public int findMaximumXORV2(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int temp = nums[i] ^ nums[j];
+                if (temp > max) {
+                    max = temp;
+                }
+            }
+        }
+        return max;
+
+    }
+}
+
+
+class Trie {
+    Trie[] child;
+
+    Trie() {
+        child = new Trie[2];
+    }
+
 }
